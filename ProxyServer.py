15e48ce664e57b09d49eed19e2e3bbf484e1b4a3
Gravesky <patrickhdy@gmail.com>
2019-07-23 16:11:35 -0700

    mp2 complete - stand by for testing

--- ProxyServer.py
+++ ProxyServer.py
@@ -37,38 +37,50 @@ while 1:
 		tcpCliSock.send("HTTP/1.0 200 OK\r\n")            
 		tcpCliSock.send("Content-Type:text/html\r\n")
         # Fill in start
-        for
+        nloop = range(0,len(outputdata))
+        for k in nloop:
+        	tcpCliSock.send(outputdata[k])
+        	print('msg: Reading from the Cache.')
         # Fill in end
 			print('Read from cache')   
 	# Error handling for file not found in cache
 	except IOError:
 		if fileExist == "false": 
 			# Create a socket on the proxyserver
-            c = # Fill in start
+			# Fill in start
+            c = socket(AF_INET, SOCK_STREAM)
                 # Fill in end
 			hostn = filename.replace("www.","",1)         
 			print(hostn)                                   
 			try:
 				# Connect to the socket to port 80
                 # Fill in start
+                c.connect((hostn,80)
                 # Fill in end
 				# Create a temporary file on this socket and ask port 80 for the file requested by the client
 				fileobj = c.makefile('r', 0)               
 				fileobj.write("GET "+"http://" + filename + " HTTP/1.0\n\n")  
 				# Read the response into buffer
                 # Fill in start
+                buff_er = fileobj.readlines()
                 # Fill in end
 				# Create a new file in the cache for the requested file. Also send the response in the buffer to client socket and the corresponding file in the cache
 				tmpFile = open("./" + filename,"wb")  
                 # Fill in start
+                for line in buff_er:
+           			tmpFile.write(line)
+           			tcpCliSock.send(line)
                 # Fill in end
 			except:
 				print("Illegal request")                                               
 		else:
 			# HTTP response message for file not found
             # Fill in start
+            tcpCliSock.send("HTTP/1.1 404 Not Found\r\n")
+            tcpCliSock.send("Content-Type:text/html\r\n")
             # Fill in end
 	# Close the client and the server sockets    
 	tcpCliSock.close() 
 # Fill in start
+tcpSerSock.close()
 # Fill in end
